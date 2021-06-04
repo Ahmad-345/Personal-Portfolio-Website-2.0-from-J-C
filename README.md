70e9a86dfb2ec452dc5656569de5ac8da1b5e2f6
Ahmad-345 <shadabsadiq1995@yahoo.com>
2021-06-04 17:11:12 +0500

    version 2.0.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# Build A Responsive Personal Portfolio Website
+
+> It is done with the help of in this [YouTube tutorial](https://www.youtube.com/watch?v=8eExETYrM0c&t=37s)
+
---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+
+// Variables
+const bodyElement = document.querySelector('body');
+const menuToggle = document.querySelector('.hamburger-menu');
+const heroElement = document.querySelector('.hero');
+const navList = document.querySelector('.nav-list');
+
+// Event Listener
+menuToggle.addEventListener('click', function () {
+  bodyElement.classList.toggle('is-open');
+  heroElement.classList.add('is-open');
+});
+
+
+// Hide overlay if clicked outside
+window.addEventListener('click', function (e) {
+  let clickedElement = e.target;
+  
+  if(clickedElement.matches('.is-open')) {
+    bodyElement.classList.remove('is-open');
+    heroElement.classList.remove('is-open');
+  }
+});
+
+navList.addEventListener('click', function(e) {
+  let clickedElement = e.target;
+
+  if(clickedElement.classList[0] === "nav-link") {
+    bodyElement.classList.remove('is-open');
+  }
+});
